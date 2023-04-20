1169f45275e3bf35d16bf320e7830b15d2d3e56c
Joseph Larralde <joseph.larralde@gmail.com>
2023-04-20 23:06:36 +0200

    attempt to support float list output in max

--- include/avnd/binding/max/outputs.hpp
+++ include/avnd/binding/max/outputs.hpp
@@ -40,6 +40,17 @@ struct outputs
           outlet_anything(p, gensym(f), 0, nullptr);
         };
       }
+      else if constexpr(std::is_same_v<func_t, void(const std::vector<float>&)>)
+      {
+        call.function = [](void* ptr, const std::vector<float>& v) {
+          t_outlet* p = static_cast<t_outlet*>(ptr);
+          t_atom l[v.size()];
+          for (std::size_t i = 0; i < v.size(); ++i) {
+            atom_setfloat(l + i, v[i]);
+          }
+          outlet_list(p, gensym("list"), v.size(), &l);
+        };
+      }
     }
     else
     {
